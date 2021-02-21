935e3ce0069e49eeadee092f88c9333a62bf28df
aoifemurfe <aoifemurfe@gmail.com>
2021-02-21 23:57:36 +0000

    update product detail

--- product/views.py
+++ product/views.py
@@ -2,18 +2,30 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import scents, products
 
 
-def shop_product(request):
-    """ A view to show the product options """
+def all_products(request):
+    """ A view to show the list of products """
 
     scent = scents.objects.all()
     product = products.objects.all()
-    #product = get_object_or_404(scents, pk=product_id)
 
     context = {
         'scent': scent,
         'product': product,
-        #'product': product,
-
     }
 
     return render(request, 'product/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show the detail for the product selected """
+
+    product = get_object_or_404(products, pk=product_id)
+    scent = scents.objects.all()
+
+    context = {
+        'scent': scent,
+        'product': product,
+    }
+
+    return render(request, 'product/product_detail.html', context)
+
