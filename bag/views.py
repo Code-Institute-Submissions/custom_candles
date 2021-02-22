dbd2bcf40c18f45e3ba61d9ef7e4d25bf9f73564
aoifemurfe <aoifemurfe@gmail.com>
2021-02-22 23:13:59 +0000

    remove and update bag

--- bag/views.py
+++ bag/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
-from .models import Cart
-from product.models import products
+from django.shortcuts import render, redirect, reverse, HttpResponse
+
 
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-    cart = Cart.objects.all()
-    context = {
-        'cart': cart,
-    }
+  
 
-    return render(request, 'bag/bag.html', context)
+
+    return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
@@ -30,6 +28,38 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    scent_one = request.POST['scent_one']
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id]['scent_one_dic'][scent_one] = quantity
+    else:
+        del bag[item_id]['scent_one_dic'][scent_one]
+        if not bag[item_id]['scent_one']:
+            bag.pop(item_id)
+
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+    try:
+        scent_one = request.POST['scent_one']
+        bag = request.session.get('bag', {})
+        del bag[item_id]['scent_one_dic'][scent_one]
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
 
 
 
