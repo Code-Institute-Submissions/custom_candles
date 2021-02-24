722d34d770b96a630c33daa792dbc6838ba92996
aoifemurfe <aoifemurfe@gmail.com>
2021-02-24 18:27:57 +0000

    update toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from product.models import products
+
 
 
 
@@ -11,6 +15,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
+    product = get_object_or_404(products, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     scent_one = request.POST['scent_one_select']
@@ -19,10 +24,13 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         if scent_one in bag[item_id]['scent_one_dic'].keys():
             bag[item_id]['scent_one_dic'][scent_one] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = {'scent_one_dic': {scent_one: quantity}}
+            messages.success(request, f'Added {product.name} to your bag')
     else:
         bag[item_id] = {'scent_one_dic': {scent_one: quantity}}
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
