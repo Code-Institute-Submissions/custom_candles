f140c38c469d4f7ae06fb3d6704c21caea839fa4
aoifemurfe <aoifemurfe@gmail.com>
2021-02-24 23:56:13 +0000

    complete stripe functionality

--- checkout/admin.py
+++ checkout/admin.py
@@ -16,7 +16,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -26,7 +27,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost', 'discount',
-                    'grand_total',)
+                    'grand_total','original_bag',
+                    'stripe_pid')
 
     ordering = ('-date',)
 
