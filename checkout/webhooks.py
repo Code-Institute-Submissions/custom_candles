79b03a82303993f6cd377a706393b61c392baf3e
aoifemurfe <aoifemurfe@gmail.com>
2021-02-25 20:57:23 +0000

    bug fixes

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -17,19 +17,17 @@ def webhook(request):
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
     # Get the webhook data and verify its signature
     payload = request.body
     event = None
-    
+
     try:
       event = stripe.Event.construct_from(
-        json.loads(payload), wh_secret  
-      )
+      json.loads(payload), wh_secret
+    )
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
@@ -48,8 +46,8 @@ def webhook(request):
 
     # If there's a handler for it, get it from the event map
     # Use the generic one by default
-    
     event_handler = event_map.get(event_type, handler.handle_event)
 
+    # Call the event handler with the event
     response = event_handler(event)
-    return(response)
+    return response
