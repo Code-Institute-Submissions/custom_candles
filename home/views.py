979f6de8a38ff688a2c1e75264aa88cf3b16ca24
aoifemurfe <aoifemurfe@gmail.com>
2021-02-27 23:08:28 +0000

    update scents page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from product.models import scents, products
 
 # Create your views here.
 
@@ -6,3 +7,13 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def view_scents(request):
+    """ A view to return the index page """
+    scent = scents.objects.all()
+
+    context = {
+        'scent': scent,
+    }
+
+    return render(request, 'home/scents.html', context)
